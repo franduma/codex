f28b0cdb58d11961c9ca524ff938389a2e545b77
François Dumais <41710191+franduma@users.noreply.github.com>
2026-04-18 08:19:39 -0400

    Add runtime FR/EN switching for front-page copy

--- wordpress-theme-energies/functions.php
+++ wordpress-theme-energies/functions.php
@@ -38,12 +38,12 @@ function erp_theme_assets(): void {
 add_action('wp_enqueue_scripts', 'erp_theme_assets');
 
 function erp_primary_menu_fallback(): void {
-    wp_page_menu([
-        'menu_class'  => 'erp-menu',
-        'show_home'   => false,
-        'depth'       => 1,
-        'echo'        => true,
+    echo '<ul class="erp-menu">';
+    wp_list_pages([
+        'title_li' => '',
+        'depth'    => 1,
     ]);
+    echo '</ul>';
 }
 
 function erp_customize_register(WP_Customize_Manager $wp_customize): void {
@@ -253,3 +253,34 @@ function erp_add_body_class(array $classes): array {
     return $classes;
 }
 add_filter('body_class', 'erp_add_body_class');
+
+/**
+ * Detect if current language/locale is English.
+ */
+function erp_is_english_language(): bool {
+    if (function_exists('pll_current_language')) {
+        $language = (string) pll_current_language('slug');
+        if ($language !== '') {
+            return str_starts_with($language, 'en');
+        }
+    }
+
+    if (defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE !== '') {
+        return str_starts_with((string) ICL_LANGUAGE_CODE, 'en');
+    }
+
+    $locale = function_exists('determine_locale') ? determine_locale() : get_locale();
+
+    return str_starts_with((string) $locale, 'en');
+}
+
+/**
+ * Return FR or EN copy depending on active language.
+ */
+function erp_i18n(string $french, string $english): string {
+    if (erp_is_english_language()) {
+        return $english;
+    }
+
+    return $french;
+}
